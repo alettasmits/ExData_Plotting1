4a37bea10f4207b2c11e81897d713552118c8604
AlettaSmits <aletta.smits@hu.nl>
2015-08-07 14:06:44 +0200

    plot3

--- plot3.R
+++ plot3.R
@@ -3,7 +3,7 @@
 #####################################
 
 ##only active on my own computer
-##setwd("C:/Users/Aletta/Documents/_ALLEFILES/LOCALREPO/ExData_Plotting1/ExData_Plotting1")
+setwd("C:/Users/Aletta/Documents/_ALLEFILES/LOCALREPO/ExData_Plotting1/ExData_Plotting1")
 
 
 ############################
@@ -50,12 +50,17 @@ subsetFeb1207 <- transform(subsetFeb1207, timestamp=as.POSIXct(paste(Date, Time)
 ##PLOT THE THING
 ##############################
 
-##so finally, function plot1: 
+##so finally, function plot2: 
 ##a histogram of Global Active Power (red bars)
 ##with the main title: "Global Active Power" and x-label: "Global Active Power (kilowatts)"
-plot1 <- function() {
-  png("plot1.png")
-  hist(subsetFeb1207$Global_active_power, col ="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+plot3 <- function() {
+  png("plot3.png")
+  ##have to make sure the days of the week appear as "thu", "fri", "sat" and not in Dutch translation
+  Sys.setlocale("LC_TIME", "English")
+  plot(subsetFeb1207$timestamp, subsetFeb1207$Sub_metering_1, type="l", xlab="", ylab="Energy Submetering")
+  points(subsetFeb1207$timestamp, subsetFeb1207$Sub_metering_2, type="l", col="red")
+  points(subsetFeb1207$timestamp, subsetFeb1207$Sub_metering_3, type="l", col="blue")
+  legend("topright", col = c("black", "red", "blue"), c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1), lwd=c(1,1))         
   dev.off()
 }
-plot1()
+plot3()
